cb93f08d40c8bb09a681d6cce675c6567669bbad
1158443037@qq.com <1158443037@qq.com>
2018-07-19 11:27:25 +0800

    22j's update

--- src/Servlet/ChartServlet.java
+++ src/Servlet/ChartServlet.java
@@ -2,7 +2,6 @@ package Servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
